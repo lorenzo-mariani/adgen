fff446adcbc040ea0fb585b08ecca58a817887f0
lorenzo-mariani <marianilorenzo1362@gmail.com>
2021-11-03 08:51:15 +0100

    Moved functions from test_db.py to test_loader.py

--- tests/test_db.py
+++ tests/test_db.py
@@ -4,7 +4,6 @@
 # Copyright © 2021, Lorenzo Mariani.
 # See /LICENSE for licensing information.
 
-import os
 import sys
 import uuid
 import builtins
@@ -13,70 +12,6 @@ import adgen.db as db
 from unittest import mock
 from adgen.cl_parser import parse_args
 from adgen.initializer import initialize
-from adgen.config import DEFAULT_CONFIG
-from adgen.utils.loader import get_value_from_ini
-
-
-def test_commands():
-    # Run mode
-    run_args = [
-        "adgen",
-        "run",
-        "--url", "bolt://localhost:7687",
-        "--user", "run_user",
-        "--passwd", "run_passwd",
-        "--nodes", "400",
-        "--domain", "run_domain.local"
-    ]
-
-    with mock.patch.object(sys, 'argv', run_args):
-        args = parse_args(run_args)
-        cmd_params = vars(args)
-        entity = initialize(cmd_params)
-
-    assert entity.url == "bolt://localhost:7687"
-    assert entity.username == "run_user"
-    assert entity.password == "run_passwd"
-    assert entity.nodes == 400
-    assert entity.domain == "run_domain.local".upper()
-
-    # Config mode
-    conn_config_path = os.path.join(os.path.abspath('adgen'), 'data', 'conn_config.ini')
-    param_config_path = os.path.join(os.path.abspath('adgen'), 'data', 'param_config.ini')
-    config_args = [
-        "adgen",
-        "config",
-        "--conn", conn_config_path,
-        "--param", param_config_path
-    ]
-
-    with mock.patch.object(sys, 'argv', config_args):
-        args = parse_args(config_args)
-        cmd_params = vars(args)
-        entity = initialize(cmd_params)
-
-    assert entity.url == get_value_from_ini("CONNECTION", "url", cmd_params.get('conn'))
-    assert entity.username == get_value_from_ini("CONNECTION", "username", cmd_params.get('conn'))
-    assert entity.password == get_value_from_ini("CONNECTION", "password", cmd_params.get('conn'))
-    assert entity.domain == get_value_from_ini("CONNECTION", "domain", cmd_params.get('conn'))
-    assert entity.nodes == get_value_from_ini("CONNECTION", "nodes", cmd_params.get('conn'))
-
-    # Interactive mode
-    interactive_args = [
-        "adgen",
-        "interactive"
-    ]
-
-    with mock.patch.object(sys, 'argv', interactive_args):
-        args = parse_args(interactive_args)
-        cmd_params = vars(args)
-        entity = initialize(cmd_params)
-
-    assert entity.url == DEFAULT_CONFIG.get('url')
-    assert entity.username == DEFAULT_CONFIG.get('username')
-    assert entity.password == DEFAULT_CONFIG.get('password')
-    assert entity.nodes == DEFAULT_CONFIG.get('nodes')
-    assert entity.domain == DEFAULT_CONFIG.get('domain')
 
 
 def init_entity():
