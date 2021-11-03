420125965602cf64af2bab392c08661e0574f111
lorenzo-mariani <marianilorenzo1362@gmail.com>
2021-11-03 20:47:39 +0100

    Renamed from config.py + updated default configurations due to the creation of new entities

--- adgen/default_config.py
+++ adgen/default_config.py
@@ -4,16 +4,22 @@ from adgen.utils.loader import get_list_from_pkl, get_list_from_ini
 
 default_path = os.path.join(os.path.abspath('adgen'), 'data', 'default_config.ini')
 
-DEFAULT_CONFIG = {
+DEFAULT_DB_SETTINGS = {
     'url': 'bolt://localhost:7687',
     'username': 'neo4j',
     'password': 'neo4jpwd',
     'driver': None,
-    'connected': False,
+    'connected': False
+}
+
+DEFAULT_DOMAIN_SETTINGS = {
     'nodes': 600,
     'domain': 'TESTLAB.LOCAL',
     'current_time': int(time.time()),
-    'sid': 'S-1-5-21-883232822-274137685-4173207997',
+    'sid': 'S-1-5-21-883232822-274137685-4173207997'
+}
+
+DEFAULT_POOL = {
     'first_names': get_list_from_pkl("first.pkl"),
     'last_names': get_list_from_pkl("last.pkl"),
     'clients_os': get_list_from_ini("CLIENTS", default_path),
