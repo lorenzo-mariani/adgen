c9c5d70b2f929b49503f09fe7de00add1da58f27
lorenzo-mariani <marianilorenzo1362@gmail.com>
2021-11-09 15:31:16 +0100

    Added help section for the config mode + added new argument for the run mode

--- adgen/cl_parser.py
+++ adgen/cl_parser.py
@@ -21,20 +21,30 @@ def parse_args(args):
     subparsers.required = True
 
     # Main Commands
-    interactive_parser = subparsers.add_parser('interactive', help='Interactive mode')
-    config_parser = subparsers.add_parser('config', help='Configuration mode')
-    run_parser = subparsers.add_parser('run', help='Run mode')
+    interactive_parser = subparsers.add_parser('interactive', help='interactive mode')
+    config_parser = subparsers.add_parser('config', help='configuration mode')
+    run_parser = subparsers.add_parser('run', help='run mode')
 
     # config parser
-    config_parser.add_argument('--conn', type=str, help='', required=True)
-    config_parser.add_argument('--param', type=str, help='', required=True)
+    config_parser.add_argument('--conn', type=str, required=True, help='absolute path to the file containing the '
+                                                                       'parameters necessary for the connection to '
+                                                                       'the database, i.e., url, username, password, '
+                                                                       'nodes, domain')
+    config_parser.add_argument('--param', type=str, required=True, help='absolute path to the file containing the '
+                                                                        'list of client/server operating systems ('
+                                                                        'along with their frequencies) that adgen '
+                                                                        'will use when generating client computers '
+                                                                        'and domain controllers, as well as '
+                                                                        'information about acls, groups and ous ('
+                                                                        'along with their frequencies)')
 
     # run parser
-    run_parser.add_argument('--url', type=str, help='Database URL to connect to', required=True)
-    run_parser.add_argument('--user', type=str, help='Database Username', required=True)
-    run_parser.add_argument('--passwd', type=str, help='Database Password', required=True)
-    run_parser.add_argument('--nodes', type=int, help='Number of nodes to generate', required=True)
-    run_parser.add_argument('--domain', type=str, help='Name of the domain to generate', required=True)
+    run_parser.add_argument('--url', type=str, required=True, help='database URL to connect to')
+    run_parser.add_argument('--user', type=str, required=True, help='database Username')
+    run_parser.add_argument('--passwd', type=str, required=True, help='database Password')
+    run_parser.add_argument('--domain', type=str, required=True, help='name of the domain to generate')
+    run_parser.add_argument('--nodes-val', type=int, help='number of nodes to generate')
+    run_parser.add_argument('--nodes-distr', type=str, help='distribution of nodes to generate')
 
     if len(args) == 0:
         parser.print_help(sys.stderr)
