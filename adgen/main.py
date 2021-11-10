75a125de20349fd05cbbf07213726ac6e1889f39
lorenzo-mariani <marianilorenzo1362@gmail.com>
2021-11-10 10:58:15 +0100

    Added new check before running config mode

--- adgen/main.py
+++ adgen/main.py
@@ -2,8 +2,8 @@ import sys
 
 from adgen.cl_parser import parse_args
 from adgen.commands.interactive_mode import interactive
-from adgen.commands.run_mode import run, check_args
-from adgen.commands.config_mode import config
+from adgen.commands.run_mode import run, check_run_args
+from adgen.commands.config_mode import config, check_config_args
 
 
 def main():
@@ -16,9 +16,10 @@ def main():
         if cmd == "interactive":
             interactive(cmd_params)
         elif cmd == "run":
-            check_args(cmd_params)
+            check_run_args(cmd_params)
             run(cmd_params)
         elif cmd == "config":
+            check_config_args(cmd_params)
             config(cmd_params)
     except Exception as err:
         print("{error}".format(error=err))
